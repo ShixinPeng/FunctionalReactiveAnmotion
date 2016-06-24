36bea02698d5eb0df8adf56f209240b93c5b28d5
pengshixin <shixinpeng0429@163.com>
2016-06-24 10:01:16 +0800

    cereate subject

---
+++ FunctionalReactiveAnmation/FunctionalReactiveAnmation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunctionalReactiveAnmation
+//
+//  Created by pengshixin on 16/6/24.
+//  Copyright © 2016年 pengshixin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FunctionalReactiveAnmation/FunctionalReactiveAnmation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunctionalReactiveAnmation
+//
+//  Created by pengshixin on 16/6/24.
+//  Copyright © 2016年 pengshixin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
